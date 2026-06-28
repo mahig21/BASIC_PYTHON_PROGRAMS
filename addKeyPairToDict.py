6b75381339531a49aa3c92ae9b471a49637a31a8
mahig21 <mahigawde9321@gmail.com>
2026-06-28 19:32:31 +0530

    minor changes made to input

--- addKeyPairToDict.py
+++ addKeyPairToDict.py
@@ -4,6 +4,6 @@ students_dict = {
     "Vani": "Tennis",
     "Rahim": "Cricket"
 }
-str=input().split()
+str=input("Enter the key and value separated by a space: ").split()
 students_dict[str[0]]=str[1]
 print(students_dict)
