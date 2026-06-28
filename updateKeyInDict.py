4c6a3c19332e4bdb81c0636638d5f6adad5ae0ec
mahig21 <mahigawde9321@gmail.com>
2026-06-28 19:33:14 +0530

    minore changes made to input

--- updateKeyInDict.py
+++ updateKeyInDict.py
@@ -6,6 +6,6 @@ students_dict = {
     "Deepak": "Boxing"
 }
 
-str=input().split()
+str=input("Enter the key and value separated by a space: ").split()
 students_dict[str[0]]=str[1]
 print(students_dict)
