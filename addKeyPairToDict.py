b0c209951af6cdaaf410872b3c9339910a422735
mahig21 <mahigawde9321@gmail.com>
2026-06-28 19:25:48 +0530

    adding a key pair to existing dictionary

---
+++ addKeyPairToDict.py
@@ -0,0 +1,9 @@
+students_dict = {
+    "Ram": "Cricket",
+    "Naresh": "Football",
+    "Vani": "Tennis",
+    "Rahim": "Cricket"
+}
+str=input().split()
+students_dict[str[0]]=str[1]
+print(students_dict)
