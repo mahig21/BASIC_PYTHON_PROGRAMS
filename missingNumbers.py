4b58055fb01a00efc574e854ded12877db525109
mahig21 <mahigawde9321@gmail.com>
2026-07-01 21:47:03 +0530

    identifying and priniting the missing numbers in a sequence given bu user

---
+++ missingNumbers.py
@@ -0,0 +1,14 @@
+strr_no = input("Enter the numbers separated by spaces: ").split()
+newlist =[]
+ans = []
+for i in strr_no:
+    num = int(i)
+    newlist.append(num)
+for i in range(1,newlist[-1]+1):
+    if i not in newlist:
+        ans.append(i)
+ans.sort()
+print(ans)
+    
+        
+    
